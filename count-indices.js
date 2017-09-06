8f8eb6aab0e93a76d9d55dc0d7ba70b8ec92fc39
gnujim <mijung.m@gmail.com>
2017-09-06 21:58:04 +0000

    refactored return indices assignment

--- count-indices.js
+++ count-indices.js
@@ -1,16 +1,25 @@
 function countIndices(str) {
   var charsAndIndices = {};
 
-  for (var i = 0; i < str.length; i++) {
-    if (/\w/.test(str[i])) {
-      var propName = str[i];
-      if (charsAndIndices[propName]) {
-        charsAndIndices[propName].push(i);
-      } else {
-        charsAndIndices[propName] = [i];
+  for (var i in str) {
+    var propName = str[i];
+    if (/\w/.test(propName)) {
+      if (!charsAndIndices[propName]) {
+        charsAndIndices[propName] = [];
       }
+      charsAndIndices[propName].push(i);
     }
   }
+  // for (var i = 0; i < str.length; i++) {
+  //   if (/\w/.test(str[i])) {
+  //     var propName = str[i];
+  //     if (charsAndIndices[propName]) {
+  //       charsAndIndices[propName].push(i);
+  //     } else {
+  //       charsAndIndices[propName] = [i];
+  //     }
+  //   }
+  // }
   return charsAndIndices;
 }
 console.log(countIndices('lighthouse in the house'));
